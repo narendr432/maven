a00cc0564e34e9195f6bd764eb382d02041e9a6e
narendr432 <89505680+narendr432@users.noreply.github.com>
2023-11-30 20:04:34 +0530

    Update runTestcases.sh

--- scripts/runTestcases.sh
+++ scripts/runTestcases.sh
@@ -3,7 +3,7 @@
 mvn_tasks="mvn clean test"
 base_mvn_parameters="-Ptest.group=${TEST_GROUP}"
 mvn_parameters="$base_gradle_parameters"
-chmod +x # making executable
-echo "$mvn_tasks $mvn_parameters $env_url" | xargs ./mvn
+chmod +x mvnw# making executable
+echo "$mvn_tasks $mvn_parameters $env_url" | xargs mvnw
 
 
